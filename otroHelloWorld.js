9a49d5bb1dc65685a8d4d9e30b8adc6ea6a3a2f6
badercain2 <cain.bader@gmail.com>
2026-09-14 20:35:13 -0300

    pequeña resta

---
+++ otroHelloWorld.js
@@ -0,0 +1,12 @@
+// Vamos a hacer una funcion que reste
+
+print("Hola, este es un ejemplo de una función que resta dos números.");
+
+function restar(a, b) {
+  run a - b;
+}
+
+let a = Number(prompt("Ingrese el primer número:"));
+
+
+
